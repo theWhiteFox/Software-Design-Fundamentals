e5bb379b8ccf5d990bc2c28d65d1b4ad7f3c9360
Stevo5o <stevo.joc@gmail.com>
2015-06-24 19:20:30 +0100

    OOP Methods

--- Lesson5/OOP/Form1.cs
+++ Lesson5/OOP/Form1.cs
@@ -17,37 +17,53 @@ namespace OOP
             InitializeComponent();
         }
 
+        Vehicle myCar = new Vehicle();
+        
         private void button2_Click(object sender, EventArgs e)
         {
-            //Vehicle Car;
-            //Car = new Vehicle();
 
-            Vehicle myCar = new Vehicle(); // new keyword, break point /debug here
+            
+
+            // Vehicle myCar = new Vehicle(); // new keyword, break point /debug here
             myCar.Make = "DeLorean";
             myCar.Model = "Time Machine";
             myCar.ElapsedMilage = 9999969;
 
-            string result;
-            result = myCar.Drive(30);
-            MessageBox.Show(result);
+            if(myCar.Make == "DeLorean")
+            {
+                string myModel;
+                myModel = myCar.Model;
+            }
 
-            //MessageBox.Show(myCar.Make); // break point /debug here
+            MessageBox.Show(myModel);
 
-            //int result = AddTwoNumbers(3, 5); // returns a value 
+            //string result;
+            //result = myCar.Drive(30);
+            //MessageBox.Show(result);
+
+            //MessageBox.Show(myCar.Make); // break point 
+
+            //int result = AddTwoNumbers(3, 5); // returns a value
             //// MessageBox.Show(result.ToString());
             //displayMessage(result.ToString()); // does not return a value
             //displayMessage(AddTwoNumbers(3, 9).ToString());
+
         }
 
-        public int AddTwoNumbers(int firstNumber, int secondNumber)
-        {
-            return firstNumber + secondNumber;
-        }
+        //public int AddTwoNumbers(int firstNumber, int secondNumber)
+        //{
+        //    return firstNumber + secondNumber;
+        //}
 
-        // example of void no return method
-        public void displayMessage(string message)
+        //// example of void no return method
+        //public void displayMessage(string message)
+        //{
+        //    MessageBox.Show(message);
+        //}
+
+        private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show(message);
+            MessageBox.Show(myCar.ElapsedMilage.ToString());
         }
     }
 }
