23f4145f835e95d04133ae1ca66812a863556714
Stevo5o <stevo.joc@gmail.com>
2015-06-25 20:40:18 +0100

    Read Write and Mouseclick item

---
+++ Lesson5/ReadWriteTextFile/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReadWriteTextFile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int counter = 0;
+            string line;
+            System.IO.StreamReader file = new System.IO.StreamReader(@"C:\Users\Stephen\Documents\VSProjects\Software-Design-Fundamentals\Lesson5\Values.txt");            
+            while((line = file.ReadLine()) != null)
+            {
+                listBox1.Items.Add(line);
+                counter++;
+            }
+        }
+        private void listBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            int index = this.listBox1.IndexFromPoint(e.Location);
+            if (index != System.Windows.Forms.ListBox.NoMatches)
+            {
+                MessageBox.Show(index.ToString());
+            }
+        }
+    }
+}
