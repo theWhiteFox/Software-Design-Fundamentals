919d01879e36ee448990d8523edc014644d858f0
Stevo5o <stevo.joc@gmail.com>
2015-06-25 18:32:46 +0100

    Console IO App

---
+++ Lesson5/BasicInputOut/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO; // read write to a file. Input /output
+
+//create two methods Write and Read
+
+namespace BasicInputOut
+{
+    class Program
+    {
+        static void WriteFile(string Filename)
+        {
+            File.WriteAllText(Filename, "Hello World");
+        }
+
+        static void ReadFile(string Filename)
+        {
+            Console.WriteLine(File.ReadAllText(Filename));
+        }
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {           
+
+            //System.IO // display methods
+
+            string Filename = @"mydoc.txt";
+
+            WriteFile(Filename);
+
+            ReadFile(Filename);
+
+            Console.Read();
+        }
+    }
+}
---
+++ Lesson5/CourseBooking/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO; // input & output
+
+namespace CourseBooking
+{
+    static class Program
+    {
+        static void WriteFile(string Filename)
+        {
+            File.WriteAllText(Filename, "Hello World");
+        }
+
+        static void ReadFile(string Filename)
+        {
+            Console.WriteLine(File.ReadAllText(Filename));
+        }
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+
+            //System.IO // display methods
+
+            string Filename = @"mydoc.txt";
+
+            WriteFile(Filename);
+
+            ReadFile(Filename);
+
+            Console.Read();
+
+        }
+    }
+}
