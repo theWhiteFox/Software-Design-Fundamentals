4c8e7869420e08e7ac1191f2c61bc5cfcbd80128
Stevo5o <stevo.joc@gmail.com>
2015-06-25 18:58:22 +0100

    Advanced IO

--- Lesson5/BasicInputOut/Program.cs
+++ Lesson5/BasicInputOut/Program.cs
@@ -6,20 +6,47 @@ using System.Threading.Tasks;
 
 using System.IO; // read write to a file. Input /output
 
-//create two methods Write and Read
+// create two methods Write and Read
 
 namespace BasicInputOut
 {
     class Program
     {
+        // static we can only create one instance of it
         static void WriteFile(string Filename)
         {
-            File.WriteAllText(Filename, "Hello World");
+            // a stream a river or a flow
+            FileStream fs = new FileStream(Filename, FileMode.Create, FileAccess.Write);
+
+            if (fs.CanWrite) {
+
+                // smallest size in C#
+                byte[] buffer = Encoding.ASCII.GetBytes("Hello World");
+                fs.Write(buffer, 0, buffer.Length);
+            }
+
+            fs.Flush();
+            fs.Close();
         }
 
         static void ReadFile(string Filename)
         {
-            Console.WriteLine(File.ReadAllText(Filename));
+
+            // a stream a river or a flow
+            FileStream fs = new FileStream(Filename, FileMode.Open, FileAccess.Read);
+
+            if (fs.CanRead)
+            {
+                                
+                byte[] buffer = new byte[fs.Length]; // a buffer big enough to hold all of the file
+
+                int bytesread = fs.Read(buffer, 0, buffer.Length);
+
+                Console.WriteLine(Encoding.ASCII.GetString(buffer, 0, bytesread));
+
+            }           
+
+            fs.Close();
         }
 
         /// <summary>
