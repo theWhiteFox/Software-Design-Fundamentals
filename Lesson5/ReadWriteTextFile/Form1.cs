8180653ae12300343d007ee89125b6de0fa7abb8
Stevo5o <stevo.joc@gmail.com>
2015-06-29 19:35:38 +0100

    Create open file location. Create and write to file

--- Lesson5/ReadWriteTextFile/Form1.cs
+++ Lesson5/ReadWriteTextFile/Form1.cs
@@ -40,14 +40,60 @@ namespace ReadWriteTextFile
             {
                 // write error
                 MessageBox.Show("File not found");
-            } 
-           
+            }
+
         }
         private void listBox1_MouseDoubleClick(object sender, MouseEventArgs e)
-        {            
+        {
             // get the value of the selected item
             string name = listBox1.GetItemText(listBox1.SelectedItem);
-            MessageBox.Show(name);           
+            MessageBox.Show(name);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Stream myStream;
+
+            // initializes a new instance of this class
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+            // gets or sets the current file name filter string, which determines the choices that appear in the "Save as file type" or "Files of type" box in the dialog box.
+            saveFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+
+            // gets or sets the index of the filter currently selected in the file dialog box.
+            saveFileDialog1.FilterIndex = 2;
+
+            // gets or sets a value indicating whether the dialog box restores the current directory before closing.
+            saveFileDialog1.RestoreDirectory = true;
+
+            // update if the user clicks OK  
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if ((myStream = saveFileDialog1.OpenFile()) != null)
+                {
+                    // code to write the stream goes here
+                    myStream.Close();
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                StreamWriter file = new StreamWriter("Values.txt");
+                file.WriteLine("Arya");
+                file.WriteLine("Ned");
+                file.WriteLine("Rob");
+                file.WriteLine("Bran");
+
+                file.Close();
+                MessageBox.Show("Done");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Cannot write");
+            }
         }
     }
 }
