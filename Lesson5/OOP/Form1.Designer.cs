ad1909555a0f8290639f5cc4151636923a0e5f07
Stevo5o <stevo.joc@gmail.com>
2015-06-30 07:41:05 +0100

    Qs for individuals

--- Lesson5/OOP/Form1.Designer.cs
+++ Lesson5/OOP/Form1.Designer.cs
@@ -30,11 +30,12 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(169, 112);
+            this.button1.Location = new System.Drawing.Point(186, 163);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -45,7 +46,7 @@
             // button2
             // 
             this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.button2.Location = new System.Drawing.Point(139, 165);
+            this.button2.Location = new System.Drawing.Point(167, 192);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(105, 48);
             this.button2.TabIndex = 1;
@@ -53,11 +54,20 @@
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(249, 121);
+            this.listBox1.TabIndex = 2;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -70,6 +80,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- Lesson5/OOP/Form1.cs
+++ Lesson5/OOP/Form1.cs
@@ -17,48 +17,44 @@ namespace OOP
             InitializeComponent();
         }
 
-        Vehicle myCar = new Vehicle();
-        
+
+
         private void button2_Click(object sender, EventArgs e)
         {
 
-            string[] game = { "Arya", "Tyrion", "Yara", "Daenerys", "Ramsay", "Joffrey" };
+            string[] singers = new string[6];
 
-            for (int i = 0; i < game.Length; i++)
+            singers[0] = "Nina Simone (f)";
+            singers[1] = "Killer Mike (m)";
+            singers[2] = "El Producto (m)";
+            singers[3] = "Lauren Mayberry (f)";
+            singers[4] = "Gonjasufi (m)";
+            singers[5] = "KiloWatts (m)";
+
+            int singer_male = 0;
+            int singer_female = 0;
+
+            for (int i = 0; i < singers.Length; i++)
             {
-                MessageBox.Show(game[i].ToString());
+
+                if (singers[i].Contains("(m)"))
+                {
+                    singer_male++;
+                }
+
+                if (singers[i].Contains("(f)"))
+                {
+                    singer_female++;
+                }
             }
 
-            foreach (var person in game)
-            {
-                MessageBox.Show(person);
-            }
+            listBox1.Items.Add("Number of male singers is " + singer_male.ToString());
 
-            // Vehicle myCar = new Vehicle(); // new keyword, break point /debug here
-            //myCar.Make = "DeLorean";
-            //myCar.Model = "Time Machine";
-            //myCar.ElapsedMilage = 9999969;
-
-            //if(myCar.Make == "DeLorean")
-            //{
-            //    string myModel;
-            //    myModel = myCar.Model;
-            //}
-
-            //MessageBox.Show(myModel);
-
-            //string result;
-            //result = myCar.Drive(30);
-            //MessageBox.Show(result);
-
-            //MessageBox.Show(myCar.Make); // break point 
-
-            //int result = AddTwoNumbers(3, 5); // returns a value
-            //// MessageBox.Show(result.ToString());
-            //displayMessage(result.ToString()); // does not return a value
-            //displayMessage(AddTwoNumbers(3, 9).ToString());
+            listBox1.Items.Add("Number of female singers is " + singer_female.ToString());
 
         }
+
+
 
         //public int AddTwoNumbers(int firstNumber, int secondNumber)
         //{
@@ -73,7 +69,7 @@ namespace OOP
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show(myCar.ElapsedMilage.ToString());
+            //MessageBox.Show(myCar.ElapsedMilage.ToString());
         }
     }
 }
